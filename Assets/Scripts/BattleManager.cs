98364a45b135f678544b4c6ca269faf413a12e23
Brandon Baggatta <baggatbt@gmail.com>
2023-07-03 10:20:13 -0400

    Finally, animations cancle
    Animations for triplehit now cancel as expected if the timing event is missed

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -27,156 +27,115 @@ public class BattleManager : MonoBehaviour
     }
 
     public IEnumerator PlayerAttackCoroutine(System.Action successCallback)
-{
-    yield return StartCoroutine(player.MoveToTarget());
-
-    
-
-    // Wait until player has reached the target
-    yield return new WaitUntil(() => player.isAttacking == false);
-    
-
-    // Check if the player is using a skill
-    if (player.currentSkill != null)
     {
-        yield return player.currentSkill.Execute(player, enemy, this);
+        yield return StartCoroutine(player.MoveToTarget());
+        yield return new WaitUntil(() => player.isAttacking == false);
+
+        if (player.currentSkill != null)
+        {
+            yield return player.currentSkill.Execute(player, enemy, this);
+        }
+        else
+        {
+            Debug.Log("Standard Attack Performed - This should not happen");
+            enemy.TakeDamage(player.damage);
+        }
+
+        yield return StartCoroutine(player.ReturnToPosition());
+
+        if (enemy.health <= 0)
+        {
+            Debug.Log("You win, let's celebrate");
+        }
+        else
+        {
+            state = BattleState.EnemyTurn;
+            EnemyAttack();
+        }
     }
-    else
+
+    public IEnumerator EnemyAttackCoroutine()
     {
-        // Perform standard attack
-        Debug.Log("Standard Attack Performed - This should not happen");
-        enemy.TakeDamage(player.damage);
-    }
-
-    yield return StartCoroutine(player.ReturnToPosition());  // Player returns to its position
-
-    if (enemy.health <= 0)
-    {
-        // Player wins the fight
-        Debug.Log("You win, let's celebrate");
-    }
-    else
-    {
-        state = BattleState.EnemyTurn;  // Set the state back to the enemy's turn as the enemy is still alive
-        EnemyAttack();
-    }
-}
-
-
-
-    IEnumerator EnemyAttackCoroutine()
-    {
-        // Enemy begins movement to target
         yield return StartCoroutine(enemy.MoveToTarget());
+        yield return new WaitUntil(() => enemy.isAttacking == false);
 
-     
-        yield return new WaitUntil(() => enemy.isAttacking == false);  // Wait until enemy has reached the target
-
-       
-        // If the enemy has a skill, execute it
         if (enemy.currentSkill != null)
         {
             Debug.Log("Enemy begins executing skill");
-            yield return enemy.currentSkill.Execute(enemy, player, this);  // Execute enemy's skill
+            yield return enemy.currentSkill.Execute(enemy, player, this);
             Debug.Log("Enemy has finished executing skill");
         }
         else
         {
-            // If the enemy has no skill, deal damage directly
             player.TakeDamage(enemy.damage);
         }
 
-        
-        yield return StartCoroutine(enemy.ReturnToPosition());  // Enemy returns to its position
-       
-        state = BattleState.PlayerTurn;  // Set the state back to player's turn after enemy's attack
+        yield return StartCoroutine(enemy.ReturnToPosition());
 
-        // Check if the player is still alive after the enemy's attack
+        state = BattleState.PlayerTurn;
+
         if (player.health <= 0)
         {
-            // Game over, player is defeated
             Debug.Log("Game Over. Player is defeated.");
         }
         else
         {
-            // If player is still alive, start player's turn
             state = BattleState.PlayerTurn;
         }
     }
-
-
 
     public void EnemyAttack()
     {
         if (!player.isAttacking && !enemy.isAttacking && state == BattleState.EnemyTurn)
         {
-            
             StartCoroutine(EnemyAttackCoroutine());
         }
     }
 
-/****DEPRECIATED
-    public void UseSkill(Skill skill, Character user, Character target)
-    {
-        if (state == BattleState.PlayerTurn && !user.isAttacking && !target.isAttacking)
-        {
-            user.currentSkill = skill;  // Set the current skill being used by the player
-            StartCoroutine(skill.Execute(user, target, this));
-            // Set the state to EnemyTurn after skill execution
-            state = BattleState.EnemyTurn;
-        }
-    }
-    */
-
-/** DEPRECIATED
-    public float activeTimeWindowDuration = 0.0f; // Default active time window duration
-
-    public void SetActiveTimeWindow(float windowDuration)
-    {
-        activeTimeWindowDuration = windowDuration;
-    }
-
-    */
-
     public IEnumerator PlayerActiveTimeEvent(float windowStart, float windowEnd, System.Action<bool> callback)
-{
-    float totalWindowDuration = windowEnd;  // Total duration is determined by the end time of the last window
-    float timer = totalWindowDuration;
-
-    while (timer > 0)
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Check the timing window for this skill
-            float elapsedTime = totalWindowDuration - timer;
-            Debug.Log(elapsedTime);
+        float totalWindowDuration = windowEnd;
+        float timer = totalWindowDuration;
+        bool successCallbackCalled = false;
 
-            if (elapsedTime >= windowStart && elapsedTime <= windowEnd)
+        while (timer > 0)
+        {
+            if (Input.GetMouseButtonDown(0))
             {
-                Debug.Log("Within window");
+                float elapsedTime = totalWindowDuration - timer;
                 Debug.Log(elapsedTime);
-                callback?.Invoke(true);  // Success within the timing window
+
+                if (elapsedTime >= windowStart && elapsedTime <= windowEnd)
+                {
+                    Debug.Log("Within window");
+                    Debug.Log(elapsedTime);
+                    callback?.Invoke(true);
+                    successCallbackCalled = true;
+                }
+                else
+                {
+                    callback?.Invoke(false);
+                    yield break;
+                }
             }
-            else
+
+            timer -= Time.deltaTime;
+
+            if (timer >= windowStart && timer <= windowEnd)
             {
-                callback?.Invoke(false);  // Failed outside the timing window
-                yield break;  // Exit the coroutine
+                Debug.Log("Starting Flash");
+                StartCoroutine(FlashWhite(player));
             }
+
+            yield return null;
         }
 
-        timer -= Time.deltaTime;
-
-        // Flash white to indicate tapping at this moment will be a success
-        if (timer >= windowStart && timer <= windowEnd)
+        // If timer has expired and success callback wasn't called, trigger failure callback
+        if (timer <= 0 && !successCallbackCalled)
         {
-            Debug.Log("Starting Flash");
-            StartCoroutine(FlashWhite(player));
+            callback?.Invoke(false);
         }
-
-        yield return null;
     }
-}
-
 
     public IEnumerator FlashWhite(Character character)
     {
--- Assets/Scripts/TripleHitSkill.cs
+++ Assets/Scripts/TripleHitSkill.cs
@@ -10,32 +10,49 @@ public class TripleHitSkill : Skill
     }
 
     public override IEnumerator Execute(Character user, Character target, BattleManager battleManager)
+{
+    // Reset the skillExecutionComplete flag at the start
+    skillExecutionComplete = false;
+
+    user.animator.SetTrigger("TripleHitTrigger");
+    yield return TimingWindow(user, target, battleManager, 0.2f, 0.5f);
+    if (!timingSuccess)
+        yield break; // Stop skill execution if timing event failed
+
+    user.animator.SetTrigger("TripleHitTrigger2");
+    yield return TimingWindow(user, target, battleManager, 1.5f, 1.8f);
+    if (!timingSuccess)
+        yield break; // Stop skill execution if timing event failed
+
+    user.animator.SetTrigger("TripleHitTrigger3");
+    yield return TimingWindow(user, target, battleManager, 2.8f, 3.3f);
+    if (!timingSuccess)
+        yield break; // Stop skill execution if timing event failed
+
+    // Set the skillExecutionComplete flag to true at the end
+    skillExecutionComplete = true;
+}
+
+
+private bool timingSuccess = false;
+
+private IEnumerator TimingWindow(Character user, Character target, BattleManager battleManager, float windowStart, float windowEnd)
+{
+    timingSuccess = false;
+    yield return battleManager.StartCoroutine(battleManager.PlayerActiveTimeEvent(windowStart, windowEnd, (result) =>
     {
-        // Reset the skillExecutionComplete flag at the start
-        skillExecutionComplete = false;
-
-        user.animator.SetTrigger("TripleHitTrigger");
-        yield return TimingWindow(user, target, battleManager, 0.2f, 0.5f);
-
-        user.animator.SetTrigger("TripleHitTrigger2");
-        yield return TimingWindow(user, target, battleManager, 1.5f, 1.8f);
-
-       // user.animator.SetTrigger("TripleHitTrigger");
-       // yield return TimingWindow(user, target, battleManager, 2.5f, 2.8f);
-
-        // Set the skillExecutionComplete flag to true at the end
-        skillExecutionComplete = true;
-    }
-
-    private IEnumerator TimingWindow(Character user, Character target, BattleManager battleManager, float windowStart, float windowEnd)
-    {
-        yield return battleManager.StartCoroutine(battleManager.PlayerActiveTimeEvent(windowStart, windowEnd, (result) =>
+        timingSuccess = result;
+        if (result)
         {
-            if (result)
-            {
-                Debug.Log("Successful Hit!");
-                target.TakeDamage(1);
-            }
-        }));
-    }
+            Debug.Log("Successful Hit!");
+            target.TakeDamage(1);
+        }
+        else
+        {
+            Debug.Log("Attempting to cancel animation!");
+            user.animator.SetTrigger("CancelAnimation"); //trigger to cancel anim in case of time event failure
+        }
+    }));
+}
+
 }
