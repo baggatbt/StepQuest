7b9400238bef7a0b6880c42d356073997ce186f9
Brandon Baggatta <baggatbt@gmail.com>
2023-09-29 16:17:39 -0400

    initial monster balancing for testing

--- Assets/Scripts/Goblin/Goblin.cs
+++ Assets/Scripts/Goblin/Goblin.cs
@@ -11,7 +11,7 @@ public class Goblin : Enemy
     {
         base.Awake();
         this.level = 1;
-        this.maxHealth = 30;
+        this.maxHealth = 30 + (this.level * 10);
         this.health = this.maxHealth;
         
     }
@@ -27,8 +27,8 @@ public class Goblin : Enemy
         this.expReward = 8;  //TODO: Add scaling
         this.goldReward = 3;
         this.attacksBeforeSpecial = 2;
-        this.damage = Mathf.RoundToInt(4 * Mathf.Pow(1.10f, level - 1)); //Increase by 10% each level, controlled by 1.10f
-        this.defensePower = 2;
+        this.damage = 6 + (this.level * 3);
+        this.defensePower = 2  + this.level;
 
         // Assign the skills
         this.normalSkill = this.skills[0]; // Normal skill
--- Assets/Scripts/Slime/Slime.cs
+++ Assets/Scripts/Slime/Slime.cs
@@ -17,8 +17,9 @@ public class Slime : Enemy
         this.goldReward = (2 * this.level);
         this.attacksBeforeSpecial = 2;
         this.damage = (2 + (this.level * 2));
-        this.maxHealth = (27 + this.level);
+        this.maxHealth = (27 + (this.level * 3));
         this.health = this.maxHealth;
+        this.defensePower = 4 + (this.level * 3);
 
         // Assign the skills
         this.normalSkill = this.skills[0]; // Normal skill
