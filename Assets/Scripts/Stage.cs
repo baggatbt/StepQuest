0cc13efb03b6b381c6f3dbca9cf5cc870f4a8363
Brandon Baggatta <baggatbt@gmail.com>
2023-11-16 11:26:34 -0500

    Stages implemented
    Change color depending on whether its locked or not.
    
    Cant start an unlocked node.
    
    Need to assign the stages ID's so that the one before it unlocks the next

--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -16,11 +16,22 @@ public class Stage : MonoBehaviour
     private void Start()
     {
         stageButton.onClick.AddListener(OnStageButtonClicked);
+        if (isUnlocked)
+        {
+            stageButton.GetComponent<Image>().color = Color.green; // Set color to green if unlocked
+        }
+        else
+        {
+            stageButton.GetComponent<Image>().color = Color.red; // Set color to red if locked
+        }
     }
 
     private void OnStageButtonClicked()
     {
+        if (stageButton.isUnlocked)
+        {
         GameManager.Instance.CurrentBattleConfig = stageBattleConfig; // Store the config in the GameManager
         SceneManager.LoadScene(battleSceneName); // Load the battle scene
+        }
     }
 }
