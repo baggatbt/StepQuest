7e06d0f02c5db0edce96a7565d0ea7f3c844e546
Brandon Baggatta <baggatbt@gmail.com>
2023-11-27 11:30:29 -0500

    Finished upgrade bars

--- Assets/Scripts/UserInterface/MainPage/UpgradeBar.cs
+++ Assets/Scripts/UserInterface/MainPage/UpgradeBar.cs
@@ -5,64 +5,59 @@ using UnityEngine.UI;
 
 public class UpgradeBar : MonoBehaviour
 {
-    public GameObject segmentPrefab; // This will use EmptyUpgradeSegment prefab
+    public GameObject emptySegmentPrefab; // Assign the EmptyUpgradeSegment prefab in the inspector
+    public GameObject filledSegmentPrefab; // Assign the FilledUpgradeSegment prefab in the inspector
     public Button upgradeButton; // Assign in the inspector
     public int maxLevel;
-    private int currentLevel = 0;
-    private Image[] segments;
-
-    // Add references to your sprites here
-    private Sprite emptySprite;
-    private Sprite filledSprite;
+    private int currentLevel;
+    private GameObject[] segments;
 
     private void Awake()
     {
-        // Load sprites from the Resources folder
-        emptySprite = Resources.Load<Sprite>("PreFab/UI Elements/EmptyUpgradeSegment");
-        filledSprite = Resources.Load<Sprite>("PreFab/UI Elements/FilledUpgradeSegment");
-
-        // Now create an array to hold the segment images
-        segments = new Image[maxLevel];
+        segments = new GameObject[maxLevel];
         InitializeSegments();
         upgradeButton.onClick.AddListener(UpgradeStat);
     }
 
     private void InitializeSegments()
     {
+        // Instantiate the empty segment prefabs
         for (int i = 0; i < maxLevel; i++)
         {
-            // Instantiate the segmentPrefab which should be the empty segment
-            GameObject newSegment = Instantiate(segmentPrefab, transform);
-            segments[i] = newSegment.GetComponent<Image>();
-            // Initially, all segments display the empty sprite
-            segments[i].sprite = emptySprite;
+            segments[i] = Instantiate(emptySegmentPrefab, transform);
         }
     }
 
     private void UpgradeStat()
+{
+    if (currentLevel < maxLevel)
     {
-        if (currentLevel < maxLevel)
-        {
-            currentLevel++;
-            UpdateBarDisplay();
-            // Increase the actual stat here
-        }
+        // Get the position, rotation, and parent from the current segment to be upgraded
+        Vector3 position = segments[currentLevel].transform.localPosition; // Use localPosition for UI elements
+        Quaternion rotation = segments[currentLevel].transform.localRotation;
+        Transform parent = segments[currentLevel].transform.parent;
 
+        // Destroy the current empty segment
+        Destroy(segments[currentLevel]);
+
+        // Instantiate a new filled segment at the correct position
+        segments[currentLevel] = Instantiate(filledSegmentPrefab, parent);
+        segments[currentLevel].transform.localPosition = position;
+        segments[currentLevel].transform.localRotation = rotation;
+        
+        // Ensure the new segment has the same sibling index so it appears in the correct order in the UI hierarchy
+        segments[currentLevel].transform.SetSiblingIndex(currentLevel);
+
+        // Increment the current level
+        currentLevel++;
+        Debug.Log("Upgraded");
+
+        // Disable the button if max level is reached
         if (currentLevel >= maxLevel)
         {
-            upgradeButton.interactable = false; // Disables the button when max level is reached
+            upgradeButton.interactable = false;
         }
     }
+}
 
-    private void UpdateBarDisplay()
-    {
-        for (int i = 0; i < currentLevel; i++)
-        {
-            // Update only the segment that corresponds to the current level
-            segments[i].sprite = filledSprite; // Change to the filled sprite
-        }
-    }
-
-    // No need for variables or methods for sprite management
-    // since they're loaded from the Resources folder at Awake
 }
