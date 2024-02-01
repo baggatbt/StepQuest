7b05f1573c464b01c4d3dcebf2018a1d3b7dd5f2
Brandon Baggatta <baggatbt@gmail.com>
2024-02-01 13:28:10 -0500

    fixed wizard not able to be removed
    added unique characterID

--- Assets/Scripts/Classes/Wizard/Wizard.cs
+++ Assets/Scripts/Classes/Wizard/Wizard.cs
@@ -28,6 +28,7 @@ public class Wizard : Companion
             this.heroExp = 0;
             this.heroSkillPoints = 0;
             this.heroStatPoints = 0;
+            this.characterIDNumber = "1";
         
         }  
     }
