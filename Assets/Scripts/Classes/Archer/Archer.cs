4a1dda1e140a1e01b3ac996d3c725f9e2510eefd
Brandon Baggatta <baggatbt@gmail.com>
2024-04-18 15:59:22 -0400

    archer growth to level 10 implemented

--- Assets/Scripts/Classes/Archer/Archer.cs
+++ Assets/Scripts/Classes/Archer/Archer.cs
@@ -37,22 +37,31 @@ public class Archer : Companion
 
     
 
-    public override void LevelUp()
+   public override void LevelUp()
     {
         if (this.heroExp >= ExpToNextLevel(this.heroLevel))
         {
             this.heroLevel++;
-            this.attackPower += 4;
-            this.maxHealth += 2;
-            this.health = this.maxHealth;
-            this.defensePower += 1;
-            this.speed += 3;
-            this.maxEnergy += 1;
-            this.energy = this.maxEnergy;
-            this.heroExp = 0;  
-            this.heroStatPoints += 3;
+            this.heroExp = 0; // Handle overflow exp
+            this.heroStatPoints += 5;
             this.heroSkillPoints += 1;
-        } 
+            
+            // Stat increases by level
+            if (this.heroLevel % 2 == 0) this.attackPower += 1; // Every 2 levels increase attack power
+            if (this.heroLevel % 3 == 0) this.maxHealth += 3; // Every 3 levels increase health by 3
+            else this.maxHealth += 2; // Other levels increase health by 2
+            if (this.heroLevel == 5 || this.heroLevel == 10) this.maxEnergy += 1; // Levels 5 and 10 increase energy
+            
+
+            //TO IMPLEMENT LATER
+            //As the levels go higher, the growth rates will rise to match.
+            //Example: past 20, it will be 2 attack power ever 2 levels
+            
+            this.health = this.maxHealth; // Refresh health to new max
+            this.energy = this.maxEnergy; // Refresh energy to new max
+            
+            Debug.Log("Hero leveled up, now level: " + this.heroLevel);
+        }
     }
 
     
