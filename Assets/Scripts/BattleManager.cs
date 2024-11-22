1bb638f24b0980e6a4403b67be90829cfa133bb6
Brandon Baggatta <baggatbt@gmail.com>
2024-11-22 15:29:37 -0500

    update

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -965,6 +965,7 @@ public class BattleManager : MonoBehaviour
         {
             if (character is Companion companion)
             {
+                companion.health = companion.maxHealth;
                 companion.SaveCharacterData();
             }
         }
